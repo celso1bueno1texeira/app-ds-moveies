1363779220af7cec0cbc7deafed47fe366d418d4
Celso Bueno <cbtbueno@gmail.com>
2022-01-10 17:38:11 -0300

    Subindo projeto.

---
+++ backend/app-movies/src/test/java/com/appmovies/appmovies/AppMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appmovies.appmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
